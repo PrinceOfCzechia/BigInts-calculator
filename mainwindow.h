849ac910db89d8501d3df4dafa6b25b06cca0ea8
Petr Král <72036926+PrinceOfCzechia@users.noreply.github.com>
2022-08-22 11:54:44 +0200

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "ui_mainwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    bool canAppendOperator();
+
+private slots:
+    // buttons to add characters
+    void on_buttonZero_clicked();
+    void on_buttonOne_clicked();
+    void on_buttonTwo_clicked();
+    void on_buttonThree_clicked();
+    void on_buttonFour_clicked();
+    void on_buttonFive_clicked();
+    void on_buttonSix_clicked();
+    void on_buttonSeven_clicked();
+    void on_buttonEight_clicked();
+    void on_buttonNine_clicked();
+    void on_plusButton_clicked();
+    void on_minusButton_clicked();
+    void on_timesButton_clicked();
+    void on_divideButton_clicked();
+    void on_powerButton_clicked();
+    void on_buttonLP_clicked();
+    void on_buttonRP_clicked();
+
+    // clear the line
+    void on_clcButton_clicked();
+
+    // evaluate input
+    void on_evalButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    // auxiliary functions for input evalutaion
+    void closeBrackets();
+    void refreshHistory();
+    QString binarize(QString input);
+    QString infix2RPN(QString input);
+};
+#endif // MAINWINDOW_H
